6e31db149c53bad5ba7d8e8628a6c0d3a115dc76
Kwajo Osei-Akoto <Kwajo@AlphaDogs-MacBook-Pro.local>
2016-04-25 17:12:57 -0400

    updated notes on config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  resources :users
+  #need to reference login page
+  #need to build profile settings page for buyer and seller
+
+  resources :categories
+  resources :preferences
+
   root to: 'static#home'
+  
+  #need to reference preferences model 
+  
+
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
 end
