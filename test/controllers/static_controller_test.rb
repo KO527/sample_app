444a9dfe56d2dc7659f9f521bbb63d33a5f8f693
KO527 <Kwajo.oseiakoto@gmail.com>
2016-05-05 17:11:11 -0400

    updated static controller test

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class StaticControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  test "should get nav" do
+  	get :home
+  	assert_response :success
+  	assert_select "title", "home"
+  	assert_select "a[href=?]", new_user_registration_path
+    	assert_select "a[href=?]", new_user_session_path  
+  end
+  test "should feature signout capability and have Category Roots" do
+  	get :home
+  	assert_response :success
+  	assert_select "a[href=?]", signout_path if user_signed_in?
+   	assert(@categories.count, 7) if user_signed_in?
+  end 
+
+end 
