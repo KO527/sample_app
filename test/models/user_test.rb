52525d5912cf4626c707e2921ed222a802fa5bef
KO527 <Kwajo.oseiakoto@gmail.com>
2016-05-07 18:31:31 -0400

    updated user_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
 	def setup
-		@user = User.new(name: "Vic Ramos", email: "VicRamos@example.com")
+	    @user = User.new(first_name: "Vic", last_name: "Ramos", email: "VicRamos@example.com", role: 'buyer')
 	end
 	test "should be valid" do 
-	    assert @user.valid
+	    assert @user.valid?
 	end
 end
