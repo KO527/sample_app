fc00355362af380f1369bbc701b9ea95a329bf3e
KO527 <Kwajo.oseiakoto@gmail.com>
2016-05-07 18:29:17 -0400

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,21 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  enum role: [:buyer, :seller]
-  enum gender: [:male, :female]
+  enum role: ["buyer", "seller"]
+  enum gender: ["male", "female"]
 
-  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i;
+  validates :first_name, presence: true, length: {maximum: 15}
+  validates :last_name, presence: true, length: {maximum: 15}
   validates :categories, presence: true, uniqueness: true, if: :seller?
-  validates :preferences, presence: true, uniqueness: {scope: :category}, if: :buyer?
+  validates :preferences, uniqueness: {scope: :categories}, if: :buyer?
   validates :email, presence: true, length: {maximum: 255}, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   # if buyer, list past preferences 
-   if :role.inspect == :buyer
+   if :role.inspect == "buyer"
    	has_many :preferences
    end
-   if :role.inspect == :seller
-    has_many :categories
+   if :role.inspect == "seller"
+    	has_many :categories
    end
   #Find a way to reference preferences as a subset of categories
   # :preferences = :categories
@@ -24,4 +26,4 @@ class User < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode
 
-  end
+end
