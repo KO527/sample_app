832a51706b6acad3819bd2f38a10a78982750ef6
KO527 <Kwajo.oseiakoto@gmail.com>
2016-12-25 08:46:47 -0500

    Add password reset

--- app/mailers/usermailer.rb
+++ app/mailers/usermailer.rb
@@ -16,9 +16,8 @@ class Usermailer < ApplicationMailer
   #
   #   en.usermailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+	@user = user
+    	mail to: user.email, subject: "Password reset"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222091152) do
+ActiveRecord::Schema.define(version: 20161224053035) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20161222091152) do
     t.string   "activation_digest"
     t.datetime "activated_at"
     t.boolean  "activated",         default: false
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
